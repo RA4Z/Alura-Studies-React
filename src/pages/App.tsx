70e97e9509ccc915243f9cc5712b19749aa0ca7a
RA4Z <rraz639@gmail.com>
2023-04-27 12:18:59 -0300

    Deletando valores padrão do programa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,18 +3,10 @@ import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss';
 import Cronometro from '../components/cronometro';
+import { ITarefa } from '../types/tarefa';
 
 function App() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: 'React',
-    tempo: '02:00:00'
-}, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-}, {
-    tarefa: 'Typescript',
-    tempo: '03:00:00'
-}]);
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
