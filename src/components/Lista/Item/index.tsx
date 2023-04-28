442f93b382bf288d560b5ed8363af46a31c75076
RA4Z <rraz639@gmail.com>
2023-04-28 12:10:12 -0300

    Trabalhando para conseguir selecionar uma tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -2,10 +2,32 @@ import React from "react";
 import style from '../Lista.module.scss'
 import { ITarefa } from "../../../types/tarefa";
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item(
+    { 
+        tarefa, 
+        tempo, 
+        selecionado, 
+        completado, 
+        id, 
+        selecionaTarefa 
+    }: Props) {
     console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
     return (
-        <li className={style.item}>
+        <li 
+            className={style.item} 
+            onClick={() => selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id
+                }
+            )}
+        >
             <h3> {tarefa} </h3>
             <span> {tempo} </span>
         </li>
